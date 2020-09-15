f61cb418a0dca2f8295e8c1a43a8de22ff4268cf
Otavio-Gouveia-03 <70581727+Otavio-Gouveia-03@users.noreply.github.com>
2020-09-15 08:23:44 -0300

    Create programa02.py

---
+++ LTP1-2020/pratica03/Prática04/programa02.py
@@ -0,0 +1,13 @@
+#Pede para o usuário digitar uma senha e validar ela com a senha secreta
+#Cria uma variável para guardar a senha
+senha_secreta = '221016'
+
+#Pede para o usuário digitar sua senha
+senha = input ('Digite a senha')
+
+#Verificar se a senha do usuário esta correta
+if senha == senha_secreta:
+  print('Acesso Consedido')
+  print('Seja Bem vindo')
+if senha != senha_secreta:
+  print('hahaha fale a palavra mágica')
