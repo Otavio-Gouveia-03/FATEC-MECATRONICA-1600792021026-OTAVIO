fb1d5aad4a75be856f97c08d4c283975904f3aab
Otavio-Gouveia-03 <70581727+Otavio-Gouveia-03@users.noreply.github.com>
2020-11-10 09:08:15 -0300

    Update exc1.py

--- LTP1-2020/pratica11/exc1.py
+++ LTP1-2020/pratica11/exc1.py
@@ -1,14 +1,15 @@
-Construa um programa que permita que o usuário informe os 3
-lados de um triângulo, A, B e C. Apresente para ele o valor
-da área do triângulo utilizando a fórmula de Hierão
+#Construa um programa que permita que o usuário informe os 3
+#lados de um triângulo, A, B e C. Apresente para ele o valor
+#da área do triângulo utilizando a fórmula de Hierão
+def semiper(A, B, C):
+  return (A + B + C)/2
+def area(A, B, C):
+  s = semiper(A, B, C)
+  return (s*(s-B)*(s-B)*(s-C))**1/2
 
 A = int(input('lado 1:'))
 B = int(input('lado 2:'))
 C = int(input('lado 3:'))
-s = (A + B + C)/2
-area = (s*(s-B)*(s-B)*(s-C))**1/2
+area_triangulo = area(A, B, C)
 
-if A > 0 and B > 0 and C > 0:
-  print("Resultado:" , area)
-else:
-  print("Você é burro")
+print(area triangulo)
